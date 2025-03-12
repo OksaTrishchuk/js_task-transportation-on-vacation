eb6589227a6ecdd9eda72cdc2835217501a53620
Oksana Trishchuk <trishchuk.oksa@gmail.com>
2025-03-12 16:53:28 +0100

    fix: correct rental cost calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,23 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return 80;
-  }
-
-  if (days === 3) {
-    return 100;
-  }
-
-  if (days >= 3 && days < 7) {
-    return 220;
-  }
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
 
   if (days >= 7) {
-    return 230;
+    totalCost -= 50;
+  } else if (days >= 3) {
+    totalCost -= 20;
   }
 
-  return 80;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
